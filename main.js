016f56efff4f9a5e4d5006c43e6214fac6a09ef4
doaamohamed2325 <mdoaa5588@gmail.com>
2024-12-24 20:34:13 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function rowSumOddNumbers(n) {
+    return Math.pow(n, 3); 
+  }
+  
+  
+  console.log(rowSumOddNumbers(1)); // 1
+  console.log(rowSumOddNumbers(2)); // 8
+  console.log(rowSumOddNumbers(3)); // 27
+  console.log(rowSumOddNumbers(4)); // 64
